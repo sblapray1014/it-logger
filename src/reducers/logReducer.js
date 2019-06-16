06201b604891159445e7ce488d3b7aa3019d26d3
Spencer LaPray <sblapray@yahoo.com>
2019-06-15 21:48:56 -0600

    full CRUD on logs

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,69 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG
+} from "../actions/types";
+
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: payload,
+        loading: false
+      };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, payload],
+        loading: false
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map(log => (log.id === payload.id ? payload : log))
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter(log => log.id !== payload),
+        loading: false
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case LOGS_ERROR:
+      console.error(payload);
+      return {
+        ...state,
+        error: payload
+      };
+    default:
+      return state;
+  }
+};
